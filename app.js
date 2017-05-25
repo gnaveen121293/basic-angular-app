bf6a92e7060aa1d8603ca60359935230ce370945
naveen121293 <gnaveen121293@gmail.com>
2017-05-25 15:40:17 +0530

    my second project

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-var app=angular.module("myapp",[]);
-app.controller("myController",["$scope",function($scope)
+var app=angular.module("myapp2",['ngRoute']);
+app.config(["$locationProvider","$routeProvider",function($locationProvider,$routeProvider)
+
+ {
+  $locationProvider.hashPrefix('')
+ $routeProvider
+  .when("/",
+    
+    {
+      templateUrl:"pages2/home.html",
+      controller:"myHomeController"
+    })
+.when("/register",
+    
+    {
+      templateUrl:"pages2/register.html",
+      controller:"myRegisterController"
+    })
+
+.when("/login",
+    
+    {
+      templateUrl:"pages2/login.html",
+      controller:"myLoginController"
+    })
+}])
+
+app.controller("myHomeController",["$scope",function($scope)
 {
-  $scope.message="hello";
-  $scope.employeeList=[
-  {
-     sno:1,name:'sundar', phone:12345 ,city:'bangalore'
-  }]
-
-
-
+  $scope.message="this is my home controller";
+}])
+app.controller("myRegisterController",["$scope",function($scope)
+{
+  $scope.message="this is my register controller";
+}])
+app.controller("myLoginController",["$scope",function($scope)
+{
+  $scope.message="this is my login controller";
 }])
